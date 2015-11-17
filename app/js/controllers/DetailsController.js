040f6834a532547d2f84618cad5743e57065552d
clareluna <clarelunamonahan@gmail.com>
2015-11-17 14:51:40 -0800

    structure build, angular start

--- app/js/controllers/DetailsController.js
+++ app/js/controllers/DetailsController.js
@@ -1,5 +1,13 @@
-module.exports = function() {
-	app.controller('DetailsController', ['$scope', function($scope) {
-		$scope.menu = ['list1', 'list2', 'list3'];
+module.exports = function(app) {
+	app.controller('DetailsController', ['$scope', 'Resource', function($scope, Resource) {
+		$scope.menu = //['list1', 'list2', 'list3'];
+		var listResource = Resource(menu);
+	
+		$scope.getList = function(){
+			listResource.getList(function(err, data) {
+				if(err) return console.log(err);
+				$scope.menu = data;
+			})
+		}
 	});
 }	
--- app/js/controllers/DropDownController.js
+++ app/js/controllers/DropDownController.js
@@ -1,5 +1,21 @@
 module.exports = function() {
-	app.controller('DropDownController', ['$scope', function($scope) {
-		$scope.menu = ['list1', 'list2', 'list3'];
+app.controller("DropDownController", function($scope) {
+	$scope.lists = [{
+		name: "list1",
+		url: "https://www.medbridgeeducation.com/api/codechallenge?list=1"
+	}, {
+		name: "list2",
+		hex: "https://www.medbridgeeducation.com/api/codechallenge?list=2"
+	}, {
+		name: "Green",
+		hex: "https://www.medbridgeeducation.com/api/codechallenge?list=3"
+	}];
+	$scope.list = "list1";
+});
+
+app.run(function($rootScope) {
+	angular.element(document).on("click", function(e) {
+		$rootScope.$broadcast("documentClicked", angular.element(e.target));
 	});
+});
 }	
